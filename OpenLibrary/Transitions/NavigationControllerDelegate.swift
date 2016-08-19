b262cc9010fc714c47fc6434208709a82b825eaa
Bob Wakefield <bob@cockleburr.net>
2016-08-19 16:13:49 -0500

    moved NavigationControllerDelegate.swift out of the MusicBrowse project where it should never have b been ITSAppUsesNonExemptEncryption=NO

---
+++ OpenLibrary/Transitions/NavigationControllerDelegate.swift
@@ -0,0 +1,104 @@
+//
+//  NavigationControllerDelegate.swift
+//  TestTransitions2
+//
+//  Created by Bob Wakefield on 12/8/15.
+//  Copyright © 2015 Bob Wakefield. All rights reserved.
+//
+
+import UIKit
+
+class NavigationControllerDelegate: NSObject, UINavigationControllerDelegate {
+
+    //weak var parent: UINavigationController?
+    
+    static var delegates = [ UINavigationController: UINavigationControllerDelegate ]()
+    
+    var transitionStack = [ZoomTransition]()
+    
+    static func addDelegateToNavController( navController: UINavigationController ) -> UINavigationControllerDelegate? {
+    
+        let delegate = navController.delegate
+//        assert( nil == delegate || delegate is NavigationControllerDelegate )
+//    
+//        if nil == delegate {
+//            
+//            delegate = NavigationControllerDelegate( navController: navController )
+//        }
+        
+        return delegate
+    }
+    
+    override init() {
+        
+        super.init()
+        
+//        print( "\(unsafeAddressOf(self)) NavigationControllerDelegate: init" )
+    }
+    
+    deinit {
+        
+//        print( "\(unsafeAddressOf(self)) NavigationControllerDelegate: deinit " )
+    }
+    
+    // MARK: transition stack maintenance
+    
+    func pushZoomTransition( zoomTransition: ZoomTransition ) {
+        
+        self.transitionStack.append( zoomTransition )
+        
+//        print( "\(unsafeAddressOf(self)) Push \(self.transitionStack.count) \(zoomTransition.description)" )
+    }
+
+    func popZoomTransition() -> ZoomTransition {
+        
+        assert( !transitionStackEmpty() )
+        
+        let zoomTransition = self.transitionStack.removeLast()
+        
+//        print( "\(unsafeAddressOf(self)) Pop \(self.transitionStack.count) \(zoomTransition.description)" )
+        
+        return zoomTransition
+    }
+    
+    func currentZoomTransition() -> ZoomTransition {
+        
+        assert( !transitionStackEmpty() )
+        
+        return self.transitionStack.last!
+    }
+    
+    func transitionStackEmpty() -> Bool {
+        
+        return self.transitionStack.isEmpty
+    }
+    
+    // MARK: UINavigationControllerDelegate
+
+    func navigationController(navigationController: UINavigationController, animationControllerForOperation operation: UINavigationControllerOperation, fromViewController fromVC: UIViewController, toViewController toVC: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        
+        assert( !transitionStackEmpty() )
+        
+        let transition = self.currentZoomTransition()
+        if .Pop == operation {
+            
+            transition.operation = operation
+            
+        } else {
+        
+            transition.addTransitionGesturesToView( toVC.view )
+        }
+
+//        print( "\(unsafeAddressOf(self)) operation: \(operation == .Push ? "Push" : "Pop") \(self.transitionStack.count)" )
+
+        return transition
+    }
+    
+    func navigationController( navigationController: UINavigationController, interactionControllerForAnimationController animationController: UIViewControllerAnimatedTransitioning ) -> UIViewControllerInteractiveTransitioning? {
+        
+        let zoomTransition = currentZoomTransition()
+            
+        return zoomTransition.interactive ? zoomTransition : nil
+    }
+    
+}
