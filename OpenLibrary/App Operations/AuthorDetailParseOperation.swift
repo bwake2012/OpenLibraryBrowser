5aaa6e07e113295118653a83594680c1063c0244
Bob Wakefield <bob@cockleburr.net>
2016-11-11 16:16:07 -0600

    Remove unused code

--- OpenLibrary/App Operations/AuthorDetailParseOperation.swift
+++ OpenLibrary/App Operations/AuthorDetailParseOperation.swift
@@ -12,140 +12,6 @@ import CoreData
 import BNRCoreDataStack
 import PSOperations
 
-/// An object to represent a parsed author search result.
-private class ParsedSearchResult: OpenLibraryObject {
-    
-    // MARK: Properties.
-
-    let key: String
-    let name: String
-    let personal_name: String
-    let birth_date: String
-    let death_date: String
-    
-    let photos: [Int]                // transformable
-    let links: [[String: String]]    // transformable
-    let bio: String
-    let alternate_names: [String]    // transformable
-    
-    let wikipedia: String
-    
-    let revision: Int64
-    let latest_revision: Int64
-    
-    let created: Date?
-    let last_modified: Date?
-    
-    let type: String
-    
-    // MARK: Class Factory
-    
-    class func fromJSON ( _ match: [String: AnyObject] ) -> ParsedSearchResult? {
-        
-        guard let key = match["key"] as? String else { return nil }
-        
-        guard let name = match["name"] as? String else { return nil }
-        
-        let personal_name = match["personal_name"] as? String ?? ""
-        
-        let birth_date = OpenLibraryObject.OLDateStamp( match["birth_date"] )
-        let death_date = OpenLibraryObject.OLDateStamp( match["death_date"] )
-        
-        let photos = OpenLibraryObject.OLIntArray( match["photos"] )
-
-        let links = OpenLibraryObject.OLLinks( match )
-        
-        let bioText = OpenLibraryObject.OLText( match["bio"] )
-        
-        let alternate_names = OpenLibraryObject.OLStringArray( match["alternate_names"] )
-        
-        let wikipedia = OpenLibraryObject.OLString( match["wikipedia"] )
-        
-        var revision = match["revision"] as? Int64
-        var latest_revision = match["latest_revision"] as? Int64
-        if nil == revision && nil != latest_revision {
-            
-            revision = latest_revision
-            
-        } else if nil == latest_revision && nil != revision {
-            
-            latest_revision = revision
-            
-        } else if nil == revision && nil == latest_revision {
-            
-            revision = Int64( 0 )
-            latest_revision = Int64( 0 )
-
-        }
-        
-        var created = OpenLibraryObject.OLTimeStamp( match["created"] )
-        var last_modified = OpenLibraryObject.OLTimeStamp( match["last_modified"] )
-        if nil == created && nil != last_modified {
-            
-            created = last_modified
-        
-        } else if nil == last_modified && nil != created {
-            
-            last_modified = created
-        }
-        
-        let type = match["type"] as? String ?? ""
-        
-        assert( nil != created )
-        assert( nil != last_modified )
-        assert( nil != revision )
-        assert( nil != latest_revision )
-        
-        return ParsedSearchResult( key: key, name: name, personal_name: personal_name, birth_date: birth_date, death_date: death_date, photos: photos, links: links, bio: bioText, alternate_names: alternate_names, wikipedia: wikipedia, revision: revision!, latest_revision: latest_revision!, created: created, last_modified: last_modified, type: type )
-    }
-
-    // MARK: Initialization
-    init(
-        key: String,
-        name: String,
-        personal_name: String,
-        birth_date: String,
-        death_date: String,
-        
-        photos: [Int],                // transformable
-        links: [[String: String]],    // transformable
-        bio: String,
-        alternate_names: [String],    // transformable
-        
-        wikipedia: String,
-        
-        revision: Int64,
-        latest_revision: Int64,
-        
-        created: Date?,
-        last_modified: Date?,
-        
-        type: String
-    ) {
-        
-        self.key = key
-        self.name = name
-        self.personal_name = personal_name
-        self.birth_date = birth_date
-        self.death_date = death_date
-        
-        self.photos = photos
-        self.links = links
-        self.bio = bio
-        self.alternate_names = alternate_names
-        
-        self.wikipedia = wikipedia
-        
-        self.revision = revision
-        self.latest_revision = latest_revision
-        
-        self.created = created
-        self.last_modified = last_modified
-        
-        self.type = type
-    }
-}
-
 /// An `Operation` to parse earthquakes out of a downloaded feed from the USGS.
 class AuthorDetailParseOperation: PSOperation {
     
@@ -153,8 +19,6 @@ class AuthorDetailParseOperation: PSOperation {
     let cacheFile: URL
     let context: NSManagedObjectContext
 
-    var searchResults = SearchResults()
-    
     var photos = [Int]()
     var localThumbURL: URL?
     var localMediumURL: URL?
@@ -258,10 +122,5 @@ class AuthorDetailParseOperation: PSOperation {
         }
 
         return error
-    }
-    
-    func Update( _ searchResults: SearchResults ) {
-        
-        self.searchResults = searchResults
     }
 }
