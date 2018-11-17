1fa8cf77fdd3d7458d6c72e2c8a29402001b2594
Bob Wakefield <bob@cockleburr.net>
2018-11-16 20:07:56 -0600

    Set the correct starting image view frame on nav pop.

---
+++ OpenLibrary/Utility/UIView+nearestCommonSuperview.swift
@@ -0,0 +1,9 @@
+//
+//  UIView+nearestCommonSuperview.swift
+//  OpenLibrary
+//
+//  Created by Bob Wakefield on 10/26/18.
+//  Copyright © 2018 Bob Wakefield. All rights reserved.
+//
+
+import Foundation
